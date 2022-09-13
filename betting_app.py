b0754341e0fa899e8ed512a87e4f70b3a9d7b9f3
Marcel Kintscher <74759601+kinathlete@users.noreply.github.com>
2022-09-13 14:50:03 +0200

    second commit

--- betting_app.py
+++ betting_app.py
@@ -1,5 +1,13 @@
 import streamlit
 
-streamlit.title('BET ON FIFA WORLD CUP 2022 IN QATAR!')
+streamlit.title('BET ON FIFA WORLD CUP 2022!')
 
-streamlit.header('Watch how you perform against your colleagues')
+streamlit.header('XXX users have already signed-up.')
+
+streamlit.subheader('1. Submit your bets.')
+streamlit.subheader('2. Watch how you perform.')
+streamlit.subheader('3. Compete with others.')
+
+streamlit.text_input('Username', max_chars=20, placeholder="Strubbi19")
+
+
