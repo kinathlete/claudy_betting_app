ff91e709976f9c9e1dccd2c8a3cebccf5e1cb7f6
Marcel Kintscher <74759601+kinathlete@users.noreply.github.com>
2022-09-13 15:00:12 +0200

    new commit

--- betting_app.py
+++ betting_app.py
@@ -4,10 +4,12 @@ streamlit.title('BET ON FIFA WORLD CUP 2022!')
 
 streamlit.header('XXX users have already signed-up.')
 
-streamlit.header('1. Submit your bets.')
-streamlit.header('2. Watch how you perform.')
-streamlit.header('3. Compete with others.')
+streamlit.header('1 Submit your bets.')
+streamlit.header('2 Watch how you perform.')
+streamlit.header('3 Compete with others.')
 
-streamlit.text_input('Username', max_chars=20, placeholder="Strubbi19")
+streamlit.header('Starting soon!')
+
+
 
 
