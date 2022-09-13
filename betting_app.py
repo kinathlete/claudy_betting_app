fa0b6c7eca7c2833442b41ba85b241962a860f2b
Marcel Kintscher <74759601+kinathlete@users.noreply.github.com>
2022-09-13 15:31:23 +0200

    style

--- betting_app.py
+++ betting_app.py
@@ -8,11 +8,11 @@ st.subheader('1 Submit your bets.')
 st.subheader('2 Watch how you perform.')
 st.subheader('3 Compete with others.')
 
-st.header('Starting on NOVEMBER 20 2022 in QATAR!')
+st.subheader('Starting on NOVEMBER 20 2022 in QATAR!')
 
 htp7 = "https://storage.cloud.google.com/fifa2022-betting-app-images/world-cup-2022-groups.jpeg"
 st.image(htp7, caption='Groups of FIFA World Cup 2022')
 
-
+st.text('Brought to you by Claudy Consulting.')
 
 
