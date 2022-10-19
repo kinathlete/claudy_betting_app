b55018cc3eee50510cd91b020ee600e285799b75
Marcel Kintscher <74759601+kinathlete@users.noreply.github.com>
2022-10-19 22:06:08 +0200

    user auth

--- betting_app.py
+++ betting_app.py
@@ -1,20 +1,110 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import snowflake.connector as cnx
+import pandas as pd
+import yaml
 
-st.title('BET ON FIFA WORLD CUP 2022!')
+st.title('MAKE YOUR PREDICTIONS FOR FIFA WORLD CUP 2022!')
 
-st.header('Bets are going to be accepted soon.')
+st.info('Predictions are accepted now. Scroll down to submit start.')
 
-st.text('1 Submit your bets.')
-st.text('2 Watch how you perform.')
-st.text('3 Compete with others.')
-
-st.text('Starting on NOVEMBER 20 2022 in QATAR!')
+st.header('Starting 20 NOVEMBER 2022 in QATAR!')
 
 htp1 = "https://storage.googleapis.com/fifa2022-betting-app-images/fifa-world-cup-2022-groups.jpeg"
 st.image(htp1, caption='Groups of FIFA World Cup 2022 | Source: sportco.io')
+
+# st.write('1 Submit your bets.')
+# st.write('2 Watch how you perform.')
+# st.write('3 Compete with others.')
+
+# st.text('The rules are:')
+# st.text('5 points for the correct World Cup Winner!')
+
+# User registration
+hashed_passwords = stauth.Hasher(['kinya1997', 'philya1919']).generate()
+print(hashed_passwords)
+
+# User authentication
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+# Give fixtures for a given round
+def get_fixtures(round):
+    with my_cnx.cursor() as my_cur:
+        fixtures = my_cur.execute(f"select * \
+                from fifa_world_cup_2022 a \
+                        inner join groups b \
+                            on b.team = a.teams_home_name \
+                                where a.league_round = '{round}' \
+                                    order by a.fixture_date asc;").fetch_pandas_all()
+        return fixtures
+
+# Insert prediction
+def insert_prediction(user_id, fixture_id, p_home, p_away):
+    with my_cnx.cursor() as my_cur:
+
+        new_entry = ''
+        existing_prediction = my_cur.execute(f"select id from user_predictions_results\
+            where user_id = {user_id} and fixture_id = {fixture_id};")
+        if existing_prediction:
+            my_cur.execute(f"update user_predictions_results\
+                set home_goals = {p_home} and away_goals = {p_away}\
+                    where user_id = {user_id} and fixture_id = {fixture_id} ;")
+        else:
+            my_cur.execute(f"insert into user_predictions_results \
+                values ('{user_id}', '{fixture_id}', '{p_home}', '{p_away}');")
+        # check if prediction exists in db now
+        new_entry = my_cur.execute(f"select id from user_predictions_results\
+            where user_id = {user_id} and fixture_id = {fixture_id};")
+        return new_entry
+
+# Listing all fixtures of the current round depending on current date
+if st.button('Make New Predictions'):
+    my_cnx = cnx.connect(**st.secrets["snowflake"])
+    fixtures = get_fixtures('Group Stage - 1')
+    # st.dataframe(fixtures)
+    # container for round 1 games
+    with st.form('user_predictions_results'):
+        for index, row in fixtures.iterrows():
+            fixture_id = row['FIXTURE_ID']
+            date = row['FIXTURE_DATE'][0:10]
+            group = "GROUP " + row['group']
+            home_team = row['TEAMS_HOME_NAME']
+            away_team = row['TEAMS_AWAY_NAME']
+            with st.container():
+                # user prediction
+                st.write(f"{date} | {group} -- {home_team} : {away_team}")
+                home, away = st.columns(2)
+                with home:
+                    home_goals = st.number_input(f"{home_team}", min_value=0, max_value=13)
+                with away:
+                    away_goals = st.number_input(f"{away_team}", min_value=0, max_value=13)
+
+        # Submit form
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write("slider", home_goals, "checkbox", away_goals)
+        # if st.button('Submit'):
+        #     with my_cnx.cursor() as my_cur:
+        #         for p in predictions:
+        #             p_home = prediction['home']
+        #             p_away = prediction['away']
+        #             p_fix_id = prediction['fixture_id']
+        #             p_entry = insert_prediction(user_id, fixture_id, p_home, p_away)
+    
+    my_cnx.close()
 
 st.subheader('Brought to you by Claudy Consulting.')
 
 htp2 = "https://storage.googleapis.com/fifa2022-betting-app-images/Claudy_Logo_PRIME_neg_RGB.png"
 st.image(htp2, width=100)
-
