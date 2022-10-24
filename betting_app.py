48e1ee3cca90c8336d4707ad444387c0791a711e
Marcel Kintscher <74759601+kinathlete@users.noreply.github.com>
2022-10-24 21:34:27 +0200

    user auth

--- betting_app.py
+++ betting_app.py
@@ -27,8 +27,7 @@ passwords = ['123','456']
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
-authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
-    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+authenticator = stauth.Authenticate(names,usernames,hashed_passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
 
 name, authentication_status = authenticator.login('Login','main')
 
